25bcea9225505fed97e8bcfafc72cf98c595c814
Timo <cobravspanda01@gmail.com>
2016-12-16 16:42:25 +0100

    dingen die nog niet werken

--- NetwProg/NetwProg/Program.cs
+++ NetwProg/NetwProg/Program.cs
@@ -10,7 +10,10 @@ namespace NetwProg
         static public int MijnPoort;
 
         static public Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
-        static public Dictionary<int, int> DistanceTable = new Dictionary<int, int>();
+        static public Dictionary<int, int> distances = new Dictionary<int, int>();
+        static public Dictionary<int, int> Du = new Dictionary<int, int>();
+        static public Dictionary<int, int> Nbu = new Dictionary<int, int>();
+        static public Dictionary<int[], int> ndisu = new Dictionary<int[], int>();
 
         static void Main(string[] args)
         {
@@ -46,6 +49,29 @@ namespace NetwProg
                         Buren[poort].Write.WriteLine(MijnPoort + ": " + delen[1]);
                 }
             }
+        }
+        static void Recompute(int v)
+        {
+            if (v == MijnPoort) { Du[v] = 0; Nbu[v] = MijnPoort; }
+            else { Nbu[v] = getBestNToV(v); }
+        }
+        static int getBestNToV(int v)
+        {
+            //Alle afstanden in ndisu die naar v gaan
+            var matches = ndisu.Where(kvp => secondPortEqualsV(kvp.Key, v));
+            //zet het minimum op eerste waarde
+            var min = matches.First();
+            //verkrijg het keyvaluepair met minimum afstand 
+            foreach (KeyValuePair<int[], int> kvp in matches)
+            {
+                if (kvp.Value < min.Value) min = kvp;
+            }
+            //return de port van de buur met de laagste afstand tot v 
+            return min.Key[0];
+        }
+        static bool secondPortEqualsV(int[] ndisuKey,int v)
+        {
+            return (ndisuKey[1] == v);
         }
     }
 }
