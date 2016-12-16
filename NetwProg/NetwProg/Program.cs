0d1efc66765e628ec152f24c136cf8e52082b88b
Timo <cobravspanda01@gmail.com>
2016-12-16 15:23:06 +0100

    stuff

--- NetwProg/NetwProg/Program.cs
+++ NetwProg/NetwProg/Program.cs
@@ -2,14 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace NetwProg
 {
     class Program
     {
+        static public int MijnPoort;
+
+        static public Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
+        static public Dictionary<int, int> DistanceTable = new Dictionary<int, int>();
+
         static void Main(string[] args)
         {
+            Console.Write("Op welke poort ben ik server? ");
+            MijnPoort = int.Parse(Console.ReadLine());
+            new Server(MijnPoort);
+
+            Console.WriteLine("Typ [verbind poortnummer] om verbinding te maken, bijvoorbeeld: verbind 1100");
+            Console.WriteLine("Typ [poortnummer bericht] om een bericht te sturen, bijvoorbeeld: 1100 hoi hoi");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.StartsWith("verbind"))
+                {
+                    int poort = int.Parse(input.Split()[1]);
+                    if (Buren.ContainsKey(poort))
+                        Console.WriteLine("Hier is al verbinding naar!");
+                    else
+                    {
+                        // Leg verbinding aan (als client)
+                        Buren.Add(poort, new Connection(poort));
+                    }
+                }
+                else
+                {
+                    // Stuur berichtje
+                    string[] delen = input.Split(new char[] { ' ' }, 2);
+                    int poort = int.Parse(delen[0]);
+                    if (!Buren.ContainsKey(poort))
+                        Console.WriteLine("Hier is al verbinding naar!");
+                    else
+                        Buren[poort].Write.WriteLine(MijnPoort + ": " + delen[1]);
+                }
+            }
         }
     }
 }
